c411e2d963599909af364723f4d2282bace347d1
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2018-10-05 10:09:12 +0200

    Improve Python model builder

--- python/ct/__init__.py
+++ python/ct/__init__.py
@@ -30,8 +30,8 @@ class Model:
         self._conflicts = {}         # (timestep, index) -> [detection_index]
         self._no_conflicts = {}      # timestep -> count
 
-        self._transitions = {}       # timestep -> (index_from, index_to, cost)
-        self._divisions = {}         # timestep -> (index_from, index_to_1, index_to_2, cost)
+        self._transitions = {}       # (timestep, index_from, index_to) -> cost
+        self._divisions = {}         # (timestep, index_from, index_to_1, index_to_2) -> cost
         self._no_incoming_edges = {} # (timestep, index) -> count
         self._no_outgoing_edges = {} # (timestep, index) -> count
 
@@ -61,6 +61,14 @@ class Model:
         for detection in detections:
             assert((timestep, detection) in self._detections)
 
+        if __debug__:
+            # Verify that subset of new conflict set is not already present.
+            for k, v in self._conflicts.items():
+                if timestep == k[0]:
+                    s1, s2 = set(detections), set(v)
+                    assert(not s1.issubset(s2))
+                    assert(not s2.issubset(s1))
+
         index = self._no_conflicts.get(timestep, 0)
         self._conflicts[(timestep, index)] = detections
         self._no_conflicts[timestep] = index + 1
@@ -74,7 +82,9 @@ class Model:
         self._inc_dict(self._no_outgoing_edges, k_left)
         self._inc_dict(self._no_incoming_edges, k_right)
 
-        self._transitions.setdefault(timestep, []).append((index_from, index_to, cost))
+        k = (timestep, index_from, index_to)
+        assert(k not in self._transitions)
+        self._transitions[k] = cost
 
     def add_division(self, timestep, index_from, index_to_1, index_to_2, cost):
         k_left = (timestep, index_from)
@@ -88,7 +98,9 @@ class Model:
         self._inc_dict(self._no_incoming_edges, k_right1)
         self._inc_dict(self._no_incoming_edges, k_right2)
 
-        self._divisions.setdefault(timestep, []).append((index_from, index_to_1, index_to_2, cost))
+        k = (timestep, index_from, index_to_1, index_to_2)
+        assert(k not in self._divisions)
+        self._divisions[k] = cost
 
     def construct_tracker(self):
         no_timesteps = max(self._no_detections.keys()) + 1
@@ -102,6 +114,7 @@ class Model:
                         self._no_outgoing_edges.get((timestep, detection), 0))
 
                 c_det, c_app, c_dis = self._detections[(timestep, detection)]
+                assert(c_det <= 0 and c_app >= 0 and c_dis >= 0)
                 detection_set_detection_cost(d, c_det, 0)
                 detection_set_appearance_cost(d, c_app)
                 detection_set_disappearance_cost(d, c_dis)
@@ -115,32 +128,51 @@ class Model:
                     tracker_add_conflict_link(t.tracker, timestep, conflict, i, d)
 
         slot_map = {}      # (timestep, detection, left/right) -> slot_index
-        for timestep, transitions in self._transitions.items():
-            for index_from, index_to, cost in transitions:
-                slot_left = self._inc_dict(slot_map, (timestep, index_from, 1))
-                slot_right = self._inc_dict(slot_map, (timestep + 1, index_to, 0))
-                detection_set_outgoing_cost(detection_map[(timestep, index_from)],
-                        slot_left, cost * .5)
-                detection_set_incoming_cost(detection_map[(timestep + 1, index_to)],
-                        slot_right, cost * .5)
-                tracker_add_transition(t.tracker, timestep, index_from, slot_left,
-                        index_to, slot_right)
+        for k, cost in self._transitions.items():
+            assert(cost >= 0)
+            timestep, index_from, index_to = k
+            slot_left = self._inc_dict(slot_map, (timestep, index_from, 1))
+            slot_right = self._inc_dict(slot_map, (timestep + 1, index_to, 0))
+            detection_set_outgoing_cost(detection_map[(timestep, index_from)],
+                    slot_left, cost * .5)
+            detection_set_incoming_cost(detection_map[(timestep + 1, index_to)],
+                    slot_right, cost * .5)
+            tracker_add_transition(t.tracker, timestep, index_from, slot_left,
+                    index_to, slot_right)
 
-        for timestep, divisions in self._divisions.items():
-            for index_from, index_to_1, index_to_2, cost in divisions:
-                slot_left = self._inc_dict(slot_map, (timestep, index_from, 1))
-                slot_right_1 = self._inc_dict(slot_map, (timestep + 1, index_to_1, 0))
-                slot_right_2 = self._inc_dict(slot_map, (timestep + 1, index_to_2, 0))
-                detection_set_outgoing_cost(detection_map[(timestep, index_from)],
-                        slot_left, cost / 3.0)
-                detection_set_incoming_cost(detection_map[(timestep + 1, index_to_1)],
-                        slot_right_1, cost / 3.0)
-                detection_set_incoming_cost(detection_map[(timestep + 1, index_to_2)],
-                        slot_right_2, cost / 3.0)
-                tracker_add_division(t.tracker, timestep, index_from, slot_left,
-                        index_to_1, slot_right_1, index_to_2, slot_right_2)
+        for k, cost in self._divisions.items():
+            assert(cost >= 0)
+            timestep, index_from, index_to_1, index_to_2 = k
+            slot_left = self._inc_dict(slot_map, (timestep, index_from, 1))
+            slot_right_1 = self._inc_dict(slot_map, (timestep + 1, index_to_1, 0))
+            slot_right_2 = self._inc_dict(slot_map, (timestep + 1, index_to_2, 0))
+            detection_set_outgoing_cost(detection_map[(timestep, index_from)],
+                    slot_left, cost / 3.0)
+            detection_set_incoming_cost(detection_map[(timestep + 1, index_to_1)],
+                    slot_right_1, cost / 3.0)
+            detection_set_incoming_cost(detection_map[(timestep + 1, index_to_2)],
+                    slot_right_2, cost / 3.0)
+            tracker_add_division(t.tracker, timestep, index_from, slot_left,
+                    index_to_1, slot_right_1, index_to_2, slot_right_2)
 
         return t
+
+    def dump(self):
+        lines = ['m = ct.Model()']
+
+        for k, v in self._detections.items():
+            lines.append('m.add_detection({}, {}, {}, {}, {})'.format(*k, *v))
+
+        for k, v in self._conflicts.items():
+            lines.append('m.add_conflict({}, {})'.format(k[0], v))
+
+        for k, v in self._transitions.items():
+            lines.append('m.add_transition({}, {}, {}, {})'.format(*k, v))
+
+        for k, v in self._divisions.items():
+            lines.append('m.add_division({}, {}, {}, {}, {})'.format(*k, v))
+
+        return '\n'.join(lines)
 
     @staticmethod
     def _inc_dict(d, k):
