c10cf79d7245cee6818101e80d24dbfd42809779
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-02-19 13:50:10 +0100

    Improve Gurobi code

--- python/ct/gurobi.py
+++ python/ct/gurobi.py
@@ -39,18 +39,23 @@ class Gurobi:
         v_incoming = []
         for i in range(self._model.no_incoming_edges(timestep, detection)):
             v = self._add_gurobi_variable(libct.detection_get_incoming_cost(factor, i))
+            v.VarName = 'incoming_{}_{}_{}'.format(timestep, detection, i)
             v_incoming.append(v)
         v = self._add_gurobi_variable(libct.detection_get_appearance_cost(factor))
+        v.VarName = 'appearance_{}_{}'.format(timestep, detection)
         v_incoming.append(v)
 
         v_outgoing = []
         for i in range(self._model.no_outgoing_edges(timestep, detection)):
             v = self._add_gurobi_variable(libct.detection_get_outgoing_cost(factor, i))
+            v.VarName = 'outgoing_{}_{}_{}'.format(timestep, detection, i)
             v_outgoing.append(v)
         v = self._add_gurobi_variable(libct.detection_get_disappearance_cost(factor))
+        v.VarName = 'disappearance_{}_{}'.format(timestep, detection)
         v_outgoing.append(v)
 
         v = self._add_gurobi_variable(libct.detection_get_detection_cost(factor))
+        v.VarName = 'detection_{}_{}'.format(timestep, detection)
         self._gurobi.addConstr(sum(v_incoming) == v)
         self._gurobi.addConstr(sum(v_outgoing) == v)
 
@@ -68,6 +73,7 @@ class Gurobi:
         #
         # To get around all this, we create our own slack variable here. :)
         v_slack = self._add_gurobi_variable()
+        v_slack.VarName = 'slack_{}_{}'.format(timestep, detection)
         self._gurobi.addConstr(v + v_slack == 1)
 
         return self.DetectionVariables(incoming=v_incoming, outgoing=v_outgoing,
@@ -83,14 +89,18 @@ class Gurobi:
         detections = self._model._conflicts[timestep, conflict]
 
         v_conflict = []
-        for detection in range(len(detections) + 1):
+        for detection in range(len(detections)):
             v = self._add_gurobi_variable(libct.conflict_get_cost(factor, detection))
+            v.VarName = 'conflict_{}_{}_{}'.format(timestep, conflict, detection)
             v_conflict.append(v)
+        v = self._add_gurobi_variable()
+        v.VarName = 'conflict_{}_{}_off'.format(timestep, conflict)
+        v_conflict.append(v)
         self._gurobi.addConstr(sum(v_conflict) == 1)
 
         return v_conflict
 
-    def construct(self, model, tracker, ilp_mode=True):
+    def construct(self, model, tracker, ilp_mode=True, timesteps=None):
         """Constructs a Gurobi LP/ILP problem.
 
         The argument `ilp_mode` determines if variables are binary or
@@ -99,6 +109,10 @@ class Gurobi:
         It is important that the `tracker` was constructed from the same
         underlying `model`. Costs are read off the `tracker` instance, so an
         intermediate reparametrization can be used as input.
+
+        By using `timesteps` you can limit the constructed models to contain
+        only a subset of all timesteps. By default (or by passing None) the
+        full model is built.
         """
         self.ilp_mode = ilp_mode
         self._model = model
@@ -106,9 +120,13 @@ class Gurobi:
         self._gurobi = gurobipy.Model()
         self._detections = {}
         self._conflicts = {}
+        self._timesteps = timesteps
+
+        if timesteps is None:
+            timesteps = [i for i in range(self._model.no_timesteps())]
 
         # Add all factors to ILP.
-        for t in range(self._model.no_timesteps()):
+        for t in timesteps:
             for d in range(self._model.no_detections(t)):
                 self._detections[t, d] = self._add_detection(t, d)
 
@@ -116,7 +134,7 @@ class Gurobi:
                 self._conflicts[t, c] = self._add_conflict(t, c)
 
         # Create constraints for conflict sets.
-        for t in range(self._model.no_timesteps()):
+        for t in timesteps:
             for c in range(self._model.no_conflicts(t)):
                 detection_indices = self._model._conflicts[t, c]
                 v_conflicts = self._conflicts[t, c]
@@ -131,6 +149,10 @@ class Gurobi:
             slot_left, slot_right, _ = v
             t_right = t_left + 1
 
+            if timesteps is not None:
+                if t_left not in timesteps or t_right not in timesteps:
+                    continue
+
             v_left = self._detections[t_left, detection_left].outgoing[slot_left]
             v_right = self._detections[t_right, detection_right].incoming[slot_right]
             self._gurobi.addConstr(v_left == v_right)
@@ -140,6 +162,10 @@ class Gurobi:
             t_left, detection_left, detection_right_1, detection_right_2 = k
             slot_left, slot_right_1, slot_right_2, _ = v
             t_right = t_left + 1
+
+            if timesteps is not None:
+                if t_left not in timesteps or t_right not in timesteps:
+                    continue
 
             v_left = self._detections[t_left, detection_left].outgoing[slot_left]
             v_right_1 = self._detections[t_right, detection_right_1].incoming[slot_right_1]
@@ -162,9 +188,11 @@ class Gurobi:
         """Reparametrizes the underlying `tracker` instance.
 
         The reparametrization is extracted from the Gurobi solver. Calling this
-        function is only allowed when (1) `ilp_mode == False`, and (2) `run()`
-        got called.
+        function is only allowed when (1) `ilp_mode == False`, (2) `run()` got
+        called, and (3) the full model was built.
         """
+        assert self._timesteps is None
+
         for t in range(self._model.no_timesteps()):
             for d in range(self._model.no_detections(t)):
                 factor = libct.tracker_get_detection(self._tracker.tracker, t, d)
