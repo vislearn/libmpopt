296405eb45f8946c1ea4ff0c3feba6f872582d0c
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-02-28 15:45:01 +0100

    Output primals to tracking.sol file

--- python/ct/__init__.py
+++ python/ct/__init__.py
@@ -1,5 +1,6 @@
 from .model import Model
 from .tracker import Tracker, construct_tracker
 from .gurobi import Gurobi
-from .jug import parse_jug_model, convert_jug_to_ct
-from .rounding import Primals, ExactNeighbourRounding, extract_primals_from_tracker
+from .jug import parse_jug_model, convert_jug_to_ct, format_jug_primals
+from .rounding import (Primals, ExactNeighbourRounding,
+    extract_primals_from_tracker, extract_primals_from_gurobi)
