f6608c9604c603518a18effb7fc4162be322f29d
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-07-18 14:00:19 +0200

    Allow node sorting

--- python/mpopt/gm/model.py
+++ python/mpopt/gm/model.py
@@ -1,5 +1,7 @@
 import numpy
 
+from ..utils import create_permutation
+
 
 class Model:
 
@@ -26,3 +28,22 @@ class Model:
         self.pairwise.append((u, v, numpy.asarray(costs, dtype=numpy.float64).reshape(l_u, l_v)))
         self.no_forward[u] += 1
         self.no_backward[v] += 1
+
+    def reorder_unaries(self, new_order):
+        new_to_old = new_order
+        old_to_new = create_permutation(new_order)
+
+        self.unaries = [self.unaries[i] for i in new_to_old]
+        self.pairwise = [(old_to_new[u], old_to_new[v], c) for u, v, c in self.pairwise]
+        self.no_forward = [self.no_forward[i] for i in new_to_old]
+        self.no_backward = [self.no_backward[i] for i in new_to_old]
+
+        for i, (u, v, c) in enumerate(self.pairwise):
+            if u > v:
+                self.pairwise[i] = (v, u, c.transpose())
+
+                self.no_forward[u] -= 1
+                self.no_backward[u] += 1
+
+                self.no_forward[v] += 1
+                self.no_backward[v] -= 1
