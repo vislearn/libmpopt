fcc2de34530f8829e5e7166d0a431989c08e5183
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-08-15 20:30:00 +0200

    Propagate changes of GM to QAP factors

--- include/mpopt/qap/uniqueness_factor.hpp
+++ include/mpopt/qap/uniqueness_factor.hpp
@@ -71,14 +71,25 @@ public:
       return std::numeric_limits<cost>::infinity();
   }
 
+  void round_independently()
+  {
+    auto it = std::min_element(costs_.cbegin(), costs_.cend());
+    primal_ = it - costs_.cbegin();
+    assert(primal_ >= 0 && primal_ < costs_.size());
+
+#ifndef NDEBUG
+    size_t counter = 0;
+    for (auto x : costs_)
+      if (dbg::are_identical(x, costs_[primal_]))
+        ++counter;
+
+    if (counter > 1)
+      std::cout << "highly similar values: " << dbg_info() << std::endl;
+#endif
+  }
+
   auto& primal() { return primal_; }
   const auto& primal() const { return primal_; }
-
-  void round_primal()
-  {
-    auto min = std::min_element(costs_.cbegin(), costs_.cend());
-    primal_ = min - costs_.cbegin();
-  }
 
 protected:
   void assert_index(const index idx) const { assert(idx >= 0 && idx < costs_.size()); }
