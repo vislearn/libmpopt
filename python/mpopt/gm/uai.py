cc7c0eac49af62655d2863667b0ec74b1eb107fd
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-09-02 18:06:02 +0200

    Add converter for DD → UAI

--- python/mpopt/gm/uai.py
+++ python/mpopt/gm/uai.py
@@ -53,3 +53,30 @@ def parse_uai(tokens):
 
 def parse_uai_file(f):
     return parse_uai(tokenize_file(f))
+
+
+def write_uai_file(model, f):
+    f.write('MARKOV\n')
+    f.write('{}\n'.format(len(model.unaries)))
+
+    f.write('{}'.format(' '.join(str(len(x)) for x in model.unaries)))
+    f.write('\n')
+
+    f.write('{}'.format(len(model.unaries) + len(model.pairwise)))
+    f.write('\n')
+
+    for u, _ in enumerate(model.unaries):
+        f.write('1 {}\n'.format(u))
+
+    for left, right, _ in model.pairwise:
+        f.write('2 {} {}\n'.format(left, right))
+
+    for costs in model.unaries:
+        f.write('\n{}\n'.format(len(costs)))
+        f.write(' '.join('{:e}'.format(c) for c in costs))
+        f.write('\n')
+
+    for left, right, costs in model.pairwise:
+        f.write('\n{}\n'.format(len(costs)))
+        f.write(' '.join('{:e}'.format(c) for c in numpy.nditer(costs, order='C')))
+        f.write('\n')
