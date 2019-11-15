73f8b4401036d7e223f28d949a253293c5398485
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2019-11-15 14:05:58 +0100

    Add Gurobi CT standard model construction

--- python/mpopt/ct/__init__.py
+++ python/mpopt/ct/__init__.py
@@ -1,6 +1,6 @@
 from .model import Model
 from .tracker import Tracker, construct_tracker
-from .gurobi import Gurobi
+from .gurobi import Gurobi, GurobiStandardModel, GurobiDecomposedModel
 from .jug import parse_jug_model, convert_jug_to_ct, format_jug_primals
 from .rounding import (Primals, ExactNeighbourRounding,
     extract_primals_from_tracker, extract_primals_from_gurobi)
--- python/mpopt/ct/gurobi.py
+++ python/mpopt/ct/gurobi.py
@@ -1,17 +1,98 @@
 import gurobipy
+from gurobipy import GRB
 from collections import namedtuple
 
 from . import libmpopt_ct as lib
 
 
-class Gurobi:
+class GurobiBase:
+
+    def __init__(self, model, ilp_mode=True):
+        self.model = model
+        self.gurobi = None
+        self.ilp_mode = False
+
+    def _add_gurobi_variable(self, obj=0.0, disable_ub=False):
+        """Adds a single Gurobi variable.
+
+        State of `ilp_mode` determines if the variable is continuous or binary.
+        """
+        kwargs = {'lb': 0.0,
+                  'ub': 1.0,
+                  'obj': obj,
+                  'vtype': GRB.BINARY if self.ilp_mode else GRB.CONTINUOUS}
+        if disable_ub:
+            del kwargs['ub']
+        return self.gurobi.addVar(**kwargs)
+
+    def construct(self):
+        raise NotImplementedError()
+
+    def update_upper_bound(self, tracker):
+        ub = tracker.evaluate_primal()
+        self.gurobi.Params.CutOff = ub
+
+    def run(self):
+        if self.gurobi is None:
+            self.construct()
+        self.gurobi.setParam('Method', 3)
+        self.gurobi.setParam('Threads', 1)
+        self.gurobi.optimize()
+
+
+class GurobiStandardModel(GurobiBase):
+
+    def construct(self):
+        detections = {}
+        incoming = {}
+        outgoing = {}
+        conflicts = {}
+        self.gurobi = gurobipy.Model()
+
+        for t in range(self.model.no_timesteps()):
+            for d in range(self.model.no_detections(t)):
+                c_det, c_app, c_dis = self.model._detections[t, d]
+                detections[t, d] = self._add_gurobi_variable(c_det)
+                incoming[t, d] = [self._add_gurobi_variable(c_app)]
+                outgoing[t, d] = [self._add_gurobi_variable(c_dis)]
+
+        for k, v in self.model._transitions.items():
+            timestep, index_from, index_to = k
+            slot_left, slot_right, cost = v
+
+            v = self._add_gurobi_variable(cost)
+            outgoing[timestep, index_from].append(v)
+            incoming[timestep+1, index_to].append(v)
+
+        for k, v in self.model._divisions.items():
+            timestep, index_from, index_to_1, index_to_2 = k
+            slot_left, slot_right_1, slot_right_2, cost = v
+
+            v = self._add_gurobi_variable(cost)
+            outgoing[timestep, index_from].append(v)
+            incoming[timestep+1, index_to_1].append(v)
+            incoming[timestep+1, index_to_2].append(v)
+
+        for t in range(self.model.no_timesteps()):
+            for d in range(self.model.no_detections(t)):
+                var_det = detections[t, d]
+
+                self.gurobi.addConstr(sum(incoming[t, d]) == var_det)
+                self.gurobi.addConstr(sum(outgoing[t, d]) == var_det)
+
+        for t in range(self.model.no_timesteps()):
+            for c in range(self.model.no_conflicts(t)):
+                variables = [detections[t, d] for d in self.model._conflicts[t, c]]
+                self.gurobi.addConstr(sum(variables) <= 1)
+
+
+class GurobiDecomposedModel(GurobiBase):
 
     DetectionVariables = namedtuple('DetectionVariables', 'incoming outgoing detection detection_slack')
 
-    def __init__(self):
-        self._model = None
-        self._gurobi = None
-        self.ilp_mode = False
+    def __init__(self, model, tracker, ilp_mode=True):
+        super().__init__(model, ilp_mode)
+        self.tracker = tracker
 
     def _add_gurobi_variable(self, obj=0.0):
         """Adds a single Gurobi variable.
@@ -23,10 +104,7 @@ class Gurobi:
         constraints are not taken into account and they don't create dual
         variables, see <https://groups.google.com/forum/#!topic/gurobi/GCJxRiHyb34>.)
         """
-        vtype = gurobipy.GRB.CONTINUOUS
-        if self.ilp_mode:
-            vtype = gurobipy.GRB.BINARY
-        return self._gurobi.addVar(lb=0, obj=obj, vtype=vtype)
+        return super()._add_gurobi_variable(obj, disable_ub=True)
 
     def _add_detection(self, timestep, detection):
         """Adds a single cell detection to the Gurobi problem.
@@ -34,11 +112,11 @@ class Gurobi:
         This method will create variables/coefficients/constraints for the
         detection and all incoming/outgoing slots.
         """
-        graph = lib.tracker_get_graph(self._tracker.tracker)
+        graph = lib.tracker_get_graph(self.tracker.tracker)
         factor = lib.graph_get_detection(graph, timestep, detection)
 
         v_incoming = []
-        for i in range(self._model.no_incoming_edges(timestep, detection)):
+        for i in range(self.model.no_incoming_edges(timestep, detection)):
             v = self._add_gurobi_variable(lib.detection_get_incoming_cost(factor, i))
             v.VarName = 'incoming_{}_{}_{}'.format(timestep, detection, i)
             v_incoming.append(v)
@@ -47,7 +125,7 @@ class Gurobi:
         v_incoming.append(v)
 
         v_outgoing = []
-        for i in range(self._model.no_outgoing_edges(timestep, detection)):
+        for i in range(self.model.no_outgoing_edges(timestep, detection)):
             v = self._add_gurobi_variable(lib.detection_get_outgoing_cost(factor, i))
             v.VarName = 'outgoing_{}_{}_{}'.format(timestep, detection, i)
             v_outgoing.append(v)
@@ -57,8 +135,8 @@ class Gurobi:
 
         v = self._add_gurobi_variable(lib.detection_get_detection_cost(factor))
         v.VarName = 'detection_{}_{}'.format(timestep, detection)
-        self._gurobi.addConstr(sum(v_incoming) == v)
-        self._gurobi.addConstr(sum(v_outgoing) == v)
+        self.gurobi.addConstr(sum(v_incoming) == v)
+        self.gurobi.addConstr(sum(v_outgoing) == v)
 
         # We would like to insert the constraint `v <= 1`, but the LP is then
         # not in standard form. Internally, Gurobi seems to create slack
@@ -75,7 +153,7 @@ class Gurobi:
         # To get around all this, we create our own slack variable here. :)
         v_slack = self._add_gurobi_variable()
         v_slack.VarName = 'slack_{}_{}'.format(timestep, detection)
-        self._gurobi.addConstr(v + v_slack == 1)
+        self.gurobi.addConstr(v + v_slack == 1)
 
         return self.DetectionVariables(incoming=v_incoming, outgoing=v_outgoing,
                                        detection=v, detection_slack=v_slack)
@@ -86,9 +164,9 @@ class Gurobi:
         This method will create variables/coefficients/constraints for the
         conflict factor.
         """
-        graph = lib.tracker_get_graph(self._tracker.tracker)
+        graph = lib.tracker_get_graph(self.tracker.tracker)
         factor = lib.graph_get_conflict(graph, timestep, conflict)
-        detections = self._model._conflicts[timestep, conflict]
+        detections = self.model._conflicts[timestep, conflict]
 
         v_conflict = []
         for detection in range(len(detections)):
@@ -98,11 +176,11 @@ class Gurobi:
         v = self._add_gurobi_variable()
         v.VarName = 'conflict_{}_{}_off'.format(timestep, conflict)
         v_conflict.append(v)
-        self._gurobi.addConstr(sum(v_conflict) == 1)
+        self.gurobi.addConstr(sum(v_conflict) == 1)
 
         return v_conflict
 
-    def construct(self, model, tracker, ilp_mode=True, timesteps=None):
+    def construct(self, timesteps=None):
         """Constructs a Gurobi LP/ILP problem.
 
         The argument `ilp_mode` determines if variables are binary or
@@ -116,37 +194,34 @@ class Gurobi:
         only a subset of all timesteps. By default (or by passing None) the
         full model is built.
         """
-        self.ilp_mode = ilp_mode
-        self._model = model
-        self._tracker = tracker
-        self._gurobi = gurobipy.Model()
+        self.gurobi = gurobipy.Model()
         self._detections = {}
         self._conflicts = {}
         self._timesteps = timesteps
 
         if timesteps is None:
-            timesteps = [i for i in range(self._model.no_timesteps())]
+            timesteps = [i for i in range(self.model.no_timesteps())]
 
         # Add all factors to ILP.
         for t in timesteps:
-            for d in range(self._model.no_detections(t)):
+            for d in range(self.model.no_detections(t)):
                 self._detections[t, d] = self._add_detection(t, d)
 
-            for c in range(self._model.no_conflicts(t)):
+            for c in range(self.model.no_conflicts(t)):
                 self._conflicts[t, c] = self._add_conflict(t, c)
 
         # Create constraints for conflict sets.
         for t in timesteps:
-            for c in range(self._model.no_conflicts(t)):
-                detection_indices = self._model._conflicts[t, c]
+            for c in range(self.model.no_conflicts(t)):
+                detection_indices = self.model._conflicts[t, c]
                 v_conflicts = self._conflicts[t, c]
                 v_detections = [self._detections[t, d].detection for d in detection_indices]
 
                 for v_conflict, v_detection in zip(v_conflicts, v_detections):
-                    self._gurobi.addConstr(v_conflict == v_detection)
+                    self.gurobi.addConstr(v_conflict == v_detection)
 
         # Create constraints between the two timesteps (transitions).
-        for k, v in self._model._transitions.items():
+        for k, v in self.model._transitions.items():
             t_left, detection_left, detection_right = k
             slot_left, slot_right, _ = v
             t_right = t_left + 1
@@ -157,10 +232,10 @@ class Gurobi:
 
             v_left = self._detections[t_left, detection_left].outgoing[slot_left]
             v_right = self._detections[t_right, detection_right].incoming[slot_right]
-            self._gurobi.addConstr(v_left == v_right)
+            self.gurobi.addConstr(v_left == v_right)
 
         # Create constraints between the two timesteps (divisions).
-        for k, v in self._model._divisions.items():
+        for k, v in self.model._divisions.items():
             t_left, detection_left, detection_right_1, detection_right_2 = k
             slot_left, slot_right_1, slot_right_2, _ = v
             t_right = t_left + 1
@@ -172,23 +247,11 @@ class Gurobi:
             v_left = self._detections[t_left, detection_left].outgoing[slot_left]
             v_right_1 = self._detections[t_right, detection_right_1].incoming[slot_right_1]
             v_right_2 = self._detections[t_right, detection_right_2].incoming[slot_right_2]
-            self._gurobi.addConstr(v_left == v_right_1)
-            self._gurobi.addConstr(v_left == v_right_2)
+            self.gurobi.addConstr(v_left == v_right_1)
+            self.gurobi.addConstr(v_left == v_right_2)
 
     def update_upper_bound(self):
-        ub = self._tracker.evaluate_primal()
-        self._gurobi.Params.CutOff = ub
-
-    def run(self):
-        """Runs the Gurobi optimizer.
-
-        Only valid after calling `construct`.
-        """
-        self._gurobi.setParam('Method', 3)
-        self._gurobi.setParam('Threads', 1)
-        self._gurobi.optimize()
-
-        print('Gurobi time:', self._gurobi.Runtime)
+        super().update_upper_bound(self.tracker)
 
     def extract_reparametrization(self):
         """Reparametrizes the underlying `tracker` instance.
@@ -198,10 +261,10 @@ class Gurobi:
         called, and (3) the full model was built.
         """
         assert self._timesteps is None
-        graph = lib.tracker_get_graph(self._tracker.tracker)
+        graph = lib.tracker_get_graph(self.tracker.tracker)
 
-        for t in range(self._model.no_timesteps()):
-            for d in range(self._model.no_detections(t)):
+        for t in range(self.model.no_timesteps()):
+            for d in range(self.model.no_detections(t)):
                 factor = lib.graph_get_detection(graph, t, d)
                 variables = self._detections[t, d]
 
@@ -209,23 +272,26 @@ class Gurobi:
                 off_cost = variables.detection_slack.RC
                 lib.detection_set_detection_cost(factor, on_cost - off_cost)
 
-                assert len(variables.incoming) == self._model.no_incoming_edges(t, d) + 1
-                for i in range(self._model.no_incoming_edges(t, d)):
+                assert len(variables.incoming) == self.model.no_incoming_edges(t, d) + 1
+                for i in range(self.model.no_incoming_edges(t, d)):
                     lib.detection_set_incoming_cost(factor, i, variables.incoming[i].RC)
                 lib.detection_set_appearance_cost(factor, variables.incoming[-1].RC)
 
-                assert len(variables.outgoing) == self._model.no_outgoing_edges(t, d) + 1
-                for i in range(self._model.no_outgoing_edges(t, d)):
+                assert len(variables.outgoing) == self.model.no_outgoing_edges(t, d) + 1
+                for i in range(self.model.no_outgoing_edges(t, d)):
                     lib.detection_set_outgoing_cost(factor, i, variables.outgoing[i].RC)
                 lib.detection_set_disappearance_cost(factor, variables.outgoing[-1].RC)
 
-            for c in range(self._model.no_conflicts(t)):
+            for c in range(self.model.no_conflicts(t)):
                 factor = lib.graph_get_conflict(graph, t, c)
                 variables = self._conflicts[t, c]
-                detections = self._model._conflicts[t, c]
+                detections = self.model._conflicts[t, c]
 
                 assert len(detections) + 1 == len(variables)
                 for i in range(len(detections)):
                     lib.conflict_set_cost(factor, i, variables[i].RC - variables[-1].RC)
 
-        assert abs(self._gurobi.ObjBound - self._tracker.lower_bound()) < 1e-4
+        assert abs(self.gurobi.ObjBound - self.tracker.lower_bound()) < 1e-4
+
+
+Gurobi = GurobiStandardModel
