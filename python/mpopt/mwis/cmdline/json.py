7517a92cef9f97c244760088dc27a66c4fac08de
Stefan Haller <stefan.haller@iwr.uni-heidelberg.de>
2024-03-06 20:54:10 +0100

    mwis: Improve cmdline parser for --timeout

--- python/mpopt/mwis/cmdline/json.py
+++ python/mpopt/mwis/cmdline/json.py
@@ -8,6 +8,17 @@ import signal
 from mpopt import utils, mwis
 
 
+def parse_time(s):
+    if s.endswith('h'):
+        return float(s[:-1]) * 3600
+    elif s.endswith('m'):
+        return float(s[:-1]) * 60
+    elif s.endswith('s'):
+        return float(s[:-1])
+    else:
+        return float(s)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='mwis_json', description='Optimizer for *.json maximum weighted independent set files.')
     parser.add_argument('-l', '--library', choices=('bregman_exp', 'bregman_log', 'temp_cont'), default='bregman_exp')
@@ -15,7 +26,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--max-batches', type=int, default=mwis.DEFAULT_MAX_BATCHES)
     parser.add_argument('-g', '--greedy-generations', type=int, default=mwis.DEFAULT_GREEDY_GENERATIONS, help='Specify number of greedy generation passes per batch.')
     parser.add_argument('-p', '--python', action='store_true', help='Use Python bregman implementation.')
-    parser.add_argument('-t', '--timeout', default=None)
+    parser.add_argument('-t', '--timeout', type=parse_time, default=None)
     parser.add_argument('--initial-temperature', type=float)
     parser.add_argument('--threshold-feasibility', type=float)
     parser.add_argument('--threshold-stability', type=float)
@@ -42,15 +53,7 @@ if __name__ == '__main__':
         solver.temperature_drop_factor(args.temperature_drop_factor)
 
     if args.timeout:
-        if args.timeout.endswith('h'):
-            timeout = float(args.timeout[:-1]) * 3600
-        elif args.timeout.endswith('m'):
-            timeout = float(args.timeout[:-1]) * 60
-        elif args.timeout.endswith('s'):
-            timeout = float(args.timeout[:-1])
-        else:
-            timeout = float(args.timeout)
-        signal.alarm(math.ceil(timeout))
+        signal.alarm(math.ceil(args.timeout))
 
     try:
         solver.run(args.batch_size, args.max_batches, args.greedy_generations)
